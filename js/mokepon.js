943b75269e8e65ea90ec342bce7e7dd0b38ef739
Haissam <126931418+Haissam22@users.noreply.github.com>
2023-07-17 14:22:27 -0500

    feat: :memo: validacion de seleccion
    validacion de seleccion de mascotas del jugador y enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,8 @@
 alert("Hola, Bienvenido Jugador!!")
 
+function radom(min,max){
+    return Math.floor(Math.random() * (max - min + 1)+ min)
+}
 const btnMascotaJugador= document.querySelector('#mascota-btn');
 
 btnMascotaJugador.addEventListener('click',selectMascotaJugador);
@@ -8,15 +11,36 @@ function selectMascotaJugador(){
     const input1= document.querySelector('#Hipodoge');
     const input2= document.querySelector('#Capipepo');
     const input3= document.querySelector('#Ratigueya');
+    const spanNMascota=document.querySelector('#nMascota');
+
 
     if(input1.checked){
         alert('seleccionaste a Hipodoge');
+        spanNMascota.innerHTML='Hipodoge';
     }else if(input2.checked){
         alert('seleccionaste a Capipepo');
+        spanNMascota.innerHTML='Capipepo';
     }else if(input3.checked){
         alert('seleccionaste a Ratigueya');
+        spanNMascota.innerHTML='Ratigueya';
     }else{
-        alert('recuerda seleccionar a alguna mascota')
+        alert('recuerda seleccionar a alguna mascota');
     }
+    selectMascotaEnemigo();
 }
 
+function selectMascotaEnemigo(){
+    const AtaqueAleatorio=radom(1,3);
+    const spanNMascota=document.querySelector('#nMascotaE');
+
+    if(AtaqueAleatorio == 1){
+        spanNMascota.innerHTML="Hipodogue"
+        //hipodogue
+    }else if( AtaqueAleatorio == 2){
+        //Capipepo
+        spanNMascota.innerHTML="Capipepo"
+    }else if(AtaqueAleatorio ==3){
+        //ratigueya
+        spanNMascota.innerHTML="Ratigueya"
+    }
+}
