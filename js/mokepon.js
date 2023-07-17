ea0d3f8a3b87cf769aa51521a0cd433a479c5515
Haissam <126931418+Haissam22@users.noreply.github.com>
2023-07-17 13:58:30 -0500

    feat: :memo: validacion
    se  agrego la validacion para saber que se a seleccionado

--- js/mokepon.js
+++ js/mokepon.js
@@ -5,6 +5,18 @@ const btnMascotaJugador= document.querySelector('#mascota-btn');
 btnMascotaJugador.addEventListener('click',selectMascotaJugador);
 
 function selectMascotaJugador(){
-    alert('seleccionaste una mascota')
+    const input1= document.querySelector('#Hipodoge');
+    const input2= document.querySelector('#Capipepo');
+    const input3= document.querySelector('#Ratigueya');
+
+    if(input1.checked){
+        alert('seleccionaste a Hipodoge');
+    }else if(input2.checked){
+        alert('seleccionaste a Capipepo');
+    }else if(input3.checked){
+        alert('seleccionaste a Ratigueya');
+    }else{
+        alert('recuerda seleccionar a alguna mascota')
+    }
 }
 
