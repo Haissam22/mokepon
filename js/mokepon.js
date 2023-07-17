085669a1ec055a6f472dda5dda2bdcb5c797779d
Haissam <126931418+Haissam22@users.noreply.github.com>
2023-07-17 13:39:05 -0500

    feat: :sparkles: basic-mokepon
    curso de programacion basica con platzi- Juego Mokepon

---
+++ js/mokepon.js
@@ -0,0 +1,10 @@
+alert("Hola, Bienvenido Jugador!!")
+
+const btnMascotaJugador= document.querySelector('#mascota-btn');
+
+btnMascotaJugador.addEventListener('click',selectMascotaJugador);
+
+function selectMascotaJugador(){
+    alert('seleccionaste una mascota')
+}
+
